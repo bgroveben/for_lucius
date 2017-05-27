f743df5ddcc85e8dab56dd9624af622d3b2ffd95
bgroveben <bgroveben@gmail.com>
2017-05-27 04:18:55 -0700

    Add code for manually cycling through images and change fade transition time

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*  Automatically change slides every five seconds
 var slideIndex = 0;
 showSlides();
 
@@ -11,4 +12,32 @@ function showSlides() {
     if (slideIndex> slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+*/
+// Manually change slides
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
